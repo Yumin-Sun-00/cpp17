d2eeeba2b3aaf61d2c4883a0211dc35332335e0e
Yumin-Sun <yuminsun@outlook.com>
2023-01-21 15:14:34 +0100

    add notes to local static objects

---
+++ cpp1720/localstatic.cpp
@@ -0,0 +1,6 @@
+// local static objects lifetime continues across calls to the function
+// they are destroyed when program ends
+
+// a local static  is initialized before the FIRST time execution
+// it is value initialized if no explicit initializer.
+// i.e build-int type will be zero.
