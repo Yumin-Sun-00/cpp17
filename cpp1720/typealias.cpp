0c7a754633931631fdf653caae74deb4526407ab
Yumin-Sun <yuminsun@outlook.com>
2023-01-22 16:56:08 +0100

    const and typealias

---
+++ cpp1720/typealias.cpp
@@ -0,0 +1,7 @@
+// when we use typealias in a declaration
+// the base type of the declaration is that typealis
+
+typedef char* pstring; //pstring is a pointer to char
+const pstring cstr = 0; // cstr is a consant pointer to char
+const pstring *ps; // ps is a pointer to const pointer to char
+
