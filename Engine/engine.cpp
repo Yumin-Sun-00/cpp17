4c0d8c6a25977b3c129c61fddb2401bf03a949a2
Yumin-Sun <yuminsun@outlook.com>
2023-01-23 23:56:16 +0100

    dev application and the static library as the same time

---
+++ Engine/engine.cpp
@@ -0,0 +1,11 @@
+#include "engine.h"
+
+#include <iostream>
+
+namespace engine{
+
+    void PrintMessage()
+    {
+        std::cout<<"Hello"<<std::endl;
+    }
+}
---
+++ Game/main.cpp
@@ -0,0 +1,8 @@
+#include "engine.h"
+
+
+int main()
+{
+    engine::PrintMessage();
+    return 0;
+}
